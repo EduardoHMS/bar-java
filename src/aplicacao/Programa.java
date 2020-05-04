2d9b2fff63a411a8799e5a707fd4c6751278a3b0
Eduardo Henrique <eduardohenriquemoreirasilva@gmail.com>
2020-05-04 18:19:31 -0300

    Relatório gerado

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -13,6 +13,8 @@ public class Programa {
 		
 		char sexo;
 		int cerveja, refri, espeto;
+		double consumo, couvert, ingresso, total;
+		
 		
 		System.out.println("Sexo: ");
 		sexo = sc.next().charAt(0);
@@ -23,6 +25,38 @@ public class Programa {
 		System.out.println("Quantidade de espetinhos: ");
 		espeto = sc.nextInt();
 		
+		
+		
+		
+		if(Character.toLowerCase(sexo) == 'f') {
+			ingresso = 8.0;
+		}else {
+			ingresso = 10.0;
+		}
+		
+		consumo = cerveja * 5.0 + refri * 3.0 + espeto * 7.0;
+		
+		if(consumo > 30) {
+			couvert = 0.0;
+		}else {
+			couvert = 4.0;
+		}
+		
+		total = ingresso + consumo + couvert;
+		
+		
+		
+		System.out.println();
+		System.out.println("RELATÓRIO:");
+		System.out.printf("Consumo = R$ %.2f%n", consumo);
+		if(couvert == 4.0) {
+			System.out.println("Couvert = R$ 4.00");
+		}else {
+			System.out.println("Isento de Couvert");
+		}
+		System.out.printf("Ingresso = R$ %.2f%n", ingresso);
+		System.out.println();
+		System.out.printf("Valor a pagar = R$ %.2f%n", total);
 		
 		sc.close();
 	}
